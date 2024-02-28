5a360176f36038c2e2498629ad63ccb34f4ff8e7
FishAres <fiseraris@gmail.com>
2024-02-27 22:06:26 -0800

    added documentation to utils used in gen_vae_utils

--- src/logging_utils.jl
+++ src/logging_utils.jl
@@ -2,19 +2,24 @@ using DrWatson
 using TensorBoardLogger
 using Logging
 
-"create tensorboard logger"
+"Extend `savename` to allow logging functions"
 savename_(args) = savename(args; allowedtypes=(Real, String, Symbol, Function))
 
+"Create tensorboard logger"
 function new_logger(path, args)
     return TBLogger("tensorboard_logs/$(path)/$(savename(args))"; min_level=Logging.Info)
 end
 
+"""
+Convenience function to create a directory wherein thou might save models
+    with a given alias. Useful when you're training with different conditions
+"""
 function get_save_dir(save_folder, alias)
     save_dir = joinpath("saved_models", save_folder, alias)
     try
         mkpath(save_dir)
     catch
-        println("directories already there :)")
+        println("directory already there :)")
     end
     return save_dir
 end
--- src/plotting_utils.jl
+++ src/plotting_utils.jl
@@ -1,22 +1,29 @@
 using Plots
 
+"""
+Convenience function for plotting a heatmap in grayscale and the right orientation
+    to view a digit
+"""
 function plot_digit(x; color=:grays, alpha=1, boundc=true, kwargs...)
     return heatmap(x[:, end:-1:1]';
-                   color=color,
-                   clim=boundc ? (0, 1) : (minimum(x), maximum(x)),
-                   axis=nothing,
-                   colorbar=false,
-                   alpha=alpha,
-                   kwargs...)
+        color=color,
+        clim=boundc ? (0, 1) : (minimum(x), maximum(x)),
+        axis=nothing,
+        colorbar=false,
+        alpha=alpha,
+        kwargs...)
 end
 
+"""
+In-place version of `plot_digit`
+"""
 function plot_digit!(x; color=:grays, alpha=1, boundc=true, kwargs...)
     return heatmap!(x[:, end:-1:1]';
-                    color=color,
-                    clim=boundc ? (0, 1) : (minimum(x), maximum(x)),
-                    # clim=(0, 1),
-                    axis=nothing,
-                    colorbar=false,
-                    alpha=alpha,
-                    kwargs...)
+        color=color,
+        clim=boundc ? (0, 1) : (minimum(x), maximum(x)),
+        # clim=(0, 1),
+        axis=nothing,
+        colorbar=false,
+        alpha=alpha,
+        kwargs...)
 end
